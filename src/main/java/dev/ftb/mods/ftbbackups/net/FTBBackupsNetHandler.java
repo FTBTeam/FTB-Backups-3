7e86491e4f0ba2f2731551ee7296b44c8bcf7c0b
Des Herriott <des.herriott@gmail.com>
2026-05-06 16:15:43 +0100

    chore: make network packets optional

--- src/main/java/dev/ftb/mods/ftbbackups/net/FTBBackupsNetHandler.java
+++ src/main/java/dev/ftb/mods/ftbbackups/net/FTBBackupsNetHandler.java
@@ -4,7 +4,7 @@ import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 
 public class FTBBackupsNetHandler {
     public static void init(final PayloadRegistrar registrar) {
-        registrar.playToClient(BackupProgressPacket.TYPE, BackupProgressPacket.STREAM_CODEC, BackupProgressPacket::handler);
-        registrar.playToClient(NotifyDisabledPacket.TYPE, NotifyDisabledPacket.STREAM_CODEC, NotifyDisabledPacket::handler);
+        registrar.optional().playToClient(BackupProgressPacket.TYPE, BackupProgressPacket.STREAM_CODEC, BackupProgressPacket::handler);
+        registrar.optional().playToClient(NotifyDisabledPacket.TYPE, NotifyDisabledPacket.STREAM_CODEC, NotifyDisabledPacket::handler);
     }
 }
